0ee16cf299b65e841da8a640c8760c7705b5c55c
alejandro.pi87@gmail.com <alejandro.pi87@gmail.com>
2025-12-03 21:41:29 +0100

    feat: Day 3

--- JS/Day2.js
+++ JS/Day2.js
@@ -33,3 +33,28 @@ function manufactureGifts(giftsToProduce) {
 
   return result;
 }
+
+const production1 = [
+  { toy: 'car', quantity: 3 },
+  { toy: 'doll', quantity: 1 },
+  { toy: 'ball', quantity: 2 }
+]
+
+const result1 = manufactureGifts(production1)
+console.log(result1)
+// ['car', 'car', 'car', 'doll', 'ball', 'ball']
+
+const production2 = [
+  { toy: 'train', quantity: 0 }, // not manufactured
+  { toy: 'bear', quantity: -2 }, // neither
+  { toy: 'game', quantity: '3' }, // careful, not valid! it's a string
+  { toy: 'puzzle', quantity: 1 }
+]
+
+const result2 = manufactureGifts(production2)
+console.log(result2)
+// ['puzzle']
+
+const production3 = []
+const result3 = manufactureGifts(production3)
+console.log(result3)
